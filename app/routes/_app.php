eb847ea8b5fe879dd35285b30daf93a696337d11
Jphn <joaopedroholandaneves@gmail.com>
2025-02-23 14:50:02 -0300

    feat(routes): host print models routes

--- app/routes/_app.php
+++ app/routes/_app.php
@@ -9,3 +9,7 @@ app()->group('/users', function () {
     app()->post('/login', 'UsersController@login');
     app()->post('/token', 'UsersController@token');
 });
+
+app()->group('/models', function () {
+    app()->apiResource('/', 'PrintModelsController');
+});
